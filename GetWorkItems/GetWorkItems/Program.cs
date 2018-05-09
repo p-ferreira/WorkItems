7a2bceff9a7e8ff319fbc38f27481fbf7fb1ab84
pedro.ferreira <pedro.ferreira@DSV-12>
2018-05-09 13:58:16 -0300

    Refactoring of program's base

--- GetWorkItems/GetWorkItems/Program.cs
+++ GetWorkItems/GetWorkItems/Program.cs
@@ -1,11 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.TeamFoundation.Client.Reporting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace GetWorkItems
 {
@@ -13,24 +9,56 @@ namespace GetWorkItems
     {
         static void Main(string[] args)
         {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
+            try
+            {
+                WorkItemsHandler handler = new WorkItemsHandler();
+                DAO dao = new DAO(Properties.Settings.Default.sqlConnectionString);
 
-            var a = new WorkItemsHandler();
-            a.GetWorkItems();
+                var workItems = handler.GetWorkItems();
 
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine(elapsedMs);
-            Console.ReadKey();
+                dao.InsertWorkItems(workItems);
+                
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
         }
 
-        //static int[] GetWorkItemsIds()
-        //{
+        static void LogError(Exception ex)
+        {
+            string error = $"Message: { ex.Message },  StackTrace: {ex.StackTrace}";
+            try
+            {
+                error += ex.InnerException != null ?
+                                        ($"Inner Exception: {ex.InnerException.Message + " " + ex.InnerException.InnerException?.Message } ")
+                                        : "";
 
-        //}
+                using (var conn = new SqlConnection(Properties.Settings.Default.sqlConnectionString))
+                {
+                    string query = $@"INSERT INTO dbo.ErrorLogs ( Error ) VALUES (@error)";
 
-        
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@error", error);                    
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                string logError = error + exp.Message;
+                string logFileName = $"log{ DateTime.Now.ToString("ddMMyyyyHHmmss") }.txt";
+                string path = Directory.GetCurrentDirectory() + "\\" + logFileName;
 
-      
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.Write(logError);
+                    sw.Close();
+                }
+            }
+        }
     }
 }
